c95717427a6a5cae19df2719c8c99aec57fc680e
Shengyu Zhang(张盛宇) <zhangshengyu@inspur.com>
2018-06-26 15:05:55 +0800

    vue作业提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <table align="center">
+      <tr>
+        <td style="width:100px"><span>用户名:</span></td>
+        <td style="width:200px"> <input style="width:200px;height:30px" v-model="username" placeholder="root"></td>
+      </tr>
+      <tr>
+        <td style="width:100px"><span>密码:</span></td>
+        <td style="width:200px"><input style="width:200px;height:30px" v-model="pwd" placeholder="inspur_123"></td>
+      </tr>
+      <br/>
+      <tr>
+        <td colspan="2">
+          <button v-on:click="login" style="width:330px;height:50px">登录</button>
+        </td>
+      </tr>
+      <br/>
+      <tr>
+        <td>{{username}}</td>
+        <td>{{pwd}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '浪潮系统-VUE登录',
+      username: "",
+      pwd: ""
+    }
+  },
+  methods:{
+    login:function (){
+      if(this.username != 'root' || this.pwd != 'inspur_123'){
+        alert("您的用户名或者密码填写错误");
+      }else{
+        this.$router.push({name:'welcome',params:{username:this.username,pwd:this.pwd}});
+      }
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <table align="center">
+      <tr>
+        <td style="width:100px"><span>用户名:</span></td>
+        <td style="width:200px"> <input style="width:200px;height:40px" v-model="username"></td>
+      </tr>
+      <tr>
+        <td style="width:100px"><span>密码:</span></td>
+        <td style="width:200px"><input style="width:200px;height:40px" v-model="pwd"></td>
+      </tr>
+      <br/>
+      <tr>
+        <td colspan="2">{{this.$route.params.username1}}
+          <button v-on:click="back" style="width:300px;height:50px">返回</button>
+        </td>
+      </tr>
+      <br/>
+       <tr>
+        <td>{{username}}</td>
+        <td>{{pwd}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  data () {
+    return {
+      msg: '恭喜你 登录成功',
+      username: "",
+      pwd: ""
+    }
+  },
+  methods:{
+    back:function (){
+      this.$router.go(-1);
+    }
+  },
+
+  created(){
+      this.username =  this.$route.params.username;
+      this.pwd = this.$route.params.pwd;
+      this.msg = '恭喜你 '+this.username+'登录成功';
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
